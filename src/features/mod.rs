4b2485a84b8310aa73ac983c5f29c7da4971ff28
Ayush Gupta <ayush_ji@outlook.com>
2021-07-04 11:42:15 +0530

    Added check for high and low values

--- src/features/mod.rs
+++ src/features/mod.rs
@@ -1,13 +1,23 @@
 extern crate rand;
 use rand::prelude::*;
+use std::process;
+
+fn check_high_low(high : i32, low : i32) {
+    if high <= low {
+        eprintln!("check your high low value");
+        process::exit(1);
+    } 
+}
 
 pub fn handle_float(high : i32, low : i32) {
+    check_high_low(high, low);
     let mut rng = rand::thread_rng();
     let y: f64 = (rng.gen::<f64>() * (high-low) as f64) + low as f64;
     println!("float:: {}", y);
 }
 
 pub fn handle_int(high : i32, low : i32) {
+    check_high_low(high, low);
     let mut rng = rand::thread_rng();
     let y: f64 = (rng.gen::<f64>() * (high-low) as f64) + low as f64;
     println!("int ::{}", y as i64);
