c0174b7fea59bdb96e6799cf32914492253b5d17
Ayush Gupta <ayush_ji@outlook.com>
2021-07-04 16:16:01 +0530

    checking branches

--- src/lib.rs
+++ src/lib.rs
@@ -3,22 +3,17 @@ pub mod logic;
 pub mod test;
 
 pub fn run(args: Vec<String>) {
-    /**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**/
-    /**/
-    let f_format = ["f".to_string(), "-f".to_string(), "--float".to_string()]; /**/
-    /**/
-    let i_format = ["i".to_string(), "-i".to_string(), "--int".to_string()]; /**/
-    /**/
-    let h_format = ["h".to_string(), "-h".to_string(), "--high".to_string()]; /**/
-    /**/
-    let l_format = ["l".to_string(), "-l".to_string(), "--low".to_string()]; /**/
-    /**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**/
+
+    let f_format = ["f".to_string(), "-f".to_string(), "--float".to_string()];
+    let i_format = ["i".to_string(), "-i".to_string(), "--int".to_string()];
+    let h_format = ["h".to_string(), "-h".to_string(), "--high".to_string()]; 
+    let l_format = ["l".to_string(), "-l".to_string(), "--low".to_string()]; 
 
     // This variable will contain parsed arguments
     let parsed = logic::parse(args);
 
     let mut high: i32 = 0;
-    let mut low: i32 = 0;
+    let mut low : i32 = 0;
     for (op, value) in parsed.iter() {
         if l_format.contains(op) {
             low = match value.parse::<i32>() {
